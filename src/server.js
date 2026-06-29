65c6659332836314835a25b6f03147541c201b9b
Serhiy Ponomarenko <ponomarenko404@gmail.com>
2026-06-29 20:04:18 +0300

    add HW tasks

---
+++ src/server.js
@@ -0,0 +1,55 @@
+import express from "express";
+import pino from 'pino-http';
+import cors from "cors";
+import 'dotenv/config';
+
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+
+
+  app.use(express.json());
+app.use(cors()); // Дозволяє запити з будь-яких джерел
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+
+app.get('/notes', (req, res) => {
+  res.status(200).json({ "message": "Retrieved all notes" });
+});
+
+
+app.get('/notes/:noteId', (req, res) => {
+  const {noteId} = req.params;
+  res.status(200).json({ "message": `Retrieved note with ID: ${noteId}` });
+});
+
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message:  err.message,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
