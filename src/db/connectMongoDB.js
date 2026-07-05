4ed9e6893b2bb42231a9f9932b0c8be974ebc1b3
Serhiy Ponomarenko <ponomarenko404@gmail.com>
2026-07-05 11:48:19 +0300

    fix URL

--- src/db/connectMongoDB.js
+++ src/db/connectMongoDB.js
@@ -6,7 +6,6 @@ try {
     const mongoUrl = process.env.MONGO_URL;
     await mongoose.connect(mongoUrl);
     console.log('✅ MongoDB connection established successfully');
-
   }
   catch (error) {
     console.error('access denied:', error.message);
