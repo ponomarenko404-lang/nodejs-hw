8571de15111d487b7ad3913b0fcff009c9023f20
Serhiy Ponomarenko <ponomarenko404@gmail.com>
2026-07-19 13:53:17 +0300

    homework tasks

--- src/models/note.js
+++ src/models/note.js
@@ -21,6 +21,11 @@ const noteSchema = new Schema(
       enum: TAGS,
       default: 'Todo',
     },
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref:'User',
+    },
   },
    {
     timestamps: true,
